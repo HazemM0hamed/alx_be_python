79a06a90f13922dc0c40ee3f7a1e5a343df3c00f
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-06-22 03:13:13 +0300

    create the file

--- basic_operations.py
+++ basic_operations.py
@@ -6,7 +6,7 @@ multiplication = number1 * number2
 print (f"Addition of {number1} and {number2} is {addition}")
 print (f"Subtraction of {number1} and {number2} is {subtraction}")
 print (f"Multiplication of {number1} and {number2} is {multiplication} ")
-python3 basic_operations.py
+
 
 
 
