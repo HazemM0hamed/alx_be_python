4302f23b0bfb7bf0db8738900a07f15d5315ad40
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-06-22 04:55:39 +0300

    add simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,8 @@
-P = 1000
-R = 0.05
-T = 3
-I = P * R * T
-print (f"The simple interest is {I}") 
+  GNU nano 7.2                   simple_interest.py                   Modified
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
