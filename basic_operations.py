65701febc33f8e8364305158070190fe8a06b8d1
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-06-22 03:50:44 +0300

    Add basic arithmetic operations script

--- basic_operations.py
+++ basic_operations.py
@@ -5,7 +5,7 @@ subtraction = number1 - number2
 multiplication = number1 * number2
 print (f"Addition of {number1} and {number2} is {addition}")
 print (f"Subtraction of {number1} and {number2} is {subtraction}")
-print (f"Multiplication of {number1} and {number2} is {multiplication} ")
+print (f"Multiplication of {number1} and {number2} is {multiplication}")
 
 
 
