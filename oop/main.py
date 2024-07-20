442916ca7a35f72c985ef4968a6c8dcd8a6ef90d
Hazem Mohamed <hazem.mohaamed98@gmail.com>
2024-07-21 02:17:33 +0300

    Exploring Polymorphism and Method Overriding

--- oop/main.py
+++ oop/main.py
@@ -1,4 +1,4 @@
-from book_class import Book
+	from book_class import Book
 
 def main():
     # Creating an instance of Book
@@ -34,6 +34,20 @@ def main():
 
     # List all books in the library
     my_library.list_books()
+
+if __name__ == "__main__":
+    main()
+from polymorphism_demo import Shape, Rectangle, Circle
+import math
+
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
 
 if __name__ == "__main__":
     main()
---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,17 @@
+class shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method")
+    
+class Rectangle(shape):
+    def __init__(self, lenght, width):
+        self.lenght = lenght
+        self.width = width
+    def area(self):
+        return {self.lenght}*{self.widht}
+    
+class Circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
